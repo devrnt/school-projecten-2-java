f3ea64541121e0609697d9946faf263c4baf6a09
sam <sam.dhondt.w1599@student.hogent.be>
2018-04-04 21:02:16 +0200

    Fix OefeningControllerTest updateOefening

--- src/domein/Oefening.java
+++ src/domein/Oefening.java
@@ -97,4 +97,24 @@ public class Oefening implements Serializable {
         this.groepsbewerkingen = groepsbewerkingen;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Oefening other = (Oefening) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    
 }
