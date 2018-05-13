a9f88ef2389de0e08d35c82be1d3e67094ed8de2
sam <sam.dhondt.w1599@student.hogent.be>
2018-05-13 20:07:32 +0200

    Implementatie groepbeheer

---
+++ Test Packages/controllers/GroepControllerTest.java
@@ -0,0 +1,154 @@
+package controllers;
+
+import domein.Groepsbewerking;
+import domein.Oefening;
+import domein.OefeningBeheer;
+import domein.OperatorEnum;
+import exceptions.NotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.collections.ObservableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import org.mockito.Mockito;
+import repository.GenericDao;
+import repository.GroepsbewerkingDao;
+
+/**
+ *
+ * @author sam
+ */
+public class GroepControllerTest {
+
+    private OefeningController controller;
+    private GenericDao<Oefening> oefeningRepo;
+    private GroepsbewerkingDao groepsbwRepo;
+
+    private Oefening oefening;
+
+    @Before
+    public void before() {
+        // aanmaken controller
+        controller = new OefeningController();
+
+        // aanmaken mocks
+        oefeningRepo = Mockito.mock(GenericDao.class);
+        groepsbwRepo = Mockito.mock(GroepsbewerkingDao.class);
+
+        // trainen mocks
+        oefening = new Oefening("opgave", 0, "feedback", "vak", new ArrayList<>(), new ArrayList<>());
+        Mockito.when(oefeningRepo.get(1)).thenReturn(oefening);
+        Mockito.when(oefeningRepo.get(2)).thenReturn(null);
+        Mockito.when(oefeningRepo.findAll()).thenReturn(
+                new ArrayList<>(
+                        Arrays.asList(
+                                new Oefening[]{
+                                    new Oefening("opgave1", 1, "feedback1", "vak1", new ArrayList<>(), new ArrayList<>()),
+                                    new Oefening("opgave2", 1, "feedback2", "vak2", new ArrayList<>(), new ArrayList<>())
+
+                                }
+                        )
+                )
+        );
+        Mockito.when(groepsbwRepo.findAll()).thenReturn(
+                new ArrayList<>(
+                        Arrays.asList(
+                                new Groepsbewerking[]{
+                                    new Groepsbewerking("gbw1", 0, OperatorEnum.optellen),
+                                    new Groepsbewerking("gbw2", 1, OperatorEnum.aftrekken)
+
+                                }
+                        )
+                )
+        );
+
+        // setter injection mocks
+        controller.getOefeningBeheer().setOefeningRepo(oefeningRepo);
+        controller.getOefeningBeheer().setGroepsbewerkingRepo(groepsbwRepo);
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="=== createOefening ===">
+    @Test
+    public void createOefening_AddsNewOefening() {
+        controller.createOefening("opgave", 0, "feedback", "vak", new ArrayList<>(), new ArrayList<>());
+        Mockito.verify(oefeningRepo).insert(Mockito.any(Oefening.class));
+    }
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== updateOefening ===">
+    @Test
+    public void updateOefening_changesAndPersistsOefening() {
+        controller.updateOefening(1, "opgaveUpdate", 0, "feedback", "vak", new ArrayList<>(), new ArrayList<>());
+        Assert.assertEquals("opgaveUpdate", oefening.getOpgave());
+        Mockito.verify(oefeningRepo).update(Mockito.any(Oefening.class));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void updateOefening_oefeningNotFound_throwsNotFoundException() {
+        controller.updateOefening(2, "opgave2", 0, "feedback", "vak", new ArrayList<>(), new ArrayList<>());
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== deleteOefening ===">
+    @Test
+    public void deleteOefening_callsDeleteInRepo() {
+        controller.deleteOefening(1);
+        Mockito.verify(oefeningRepo).delete(Mockito.any(Oefening.class));
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void deleteOefening_oefeningNotFound_throwsNotFoundException() {
+        controller.deleteOefening(2);
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== getGroepsbewerkingen ===">
+    @Test
+    public void getGroepsbewerkingen_returnsListOfGroepsbewerkingen() {
+        ObservableList<Groepsbewerking> list = controller.getGroepsbewerkingen();
+        Assert.assertEquals(2, list.size());
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== getOefening ===">
+    @Test
+    public void getOefeningen_ReturnsOefeningen() {
+        Assert.assertEquals(2, controller.getOefeningen().size());
+    }
+
+    @Test
+    public void getOefening_returnsCorrectOefening() {
+        Oefening oefening = controller.getOefening(1);
+        Assert.assertEquals(oefening, this.oefening);
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== applyFilter ===">
+    @Test
+    public void applyFilter_NoTextReturnsAll() {
+        controller.applyFilter("");
+        Assert.assertEquals(2, controller.getOefeningen().size());
+    }
+
+    @Test
+    public void applyFilter_ReturnsMatches() {
+        controller.applyFilter("opgave1");
+        Assert.assertEquals(1, controller.getOefeningen().size());
+    }
+
+    @Test
+    public void applyFilter_WithWhiteSpaceReturnsMatches() {
+        controller.applyFilter("     opgave1    ");
+        Assert.assertEquals(1, controller.getOefeningen().size());
+    }
+//</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="=== getOefeningBeheer ===">
+    @Test
+    public void getOefeningBeheer_returnsOefeningBeheer() {
+        Assert.assertTrue(controller.getOefeningBeheer() instanceof OefeningBeheer);
+    }
+    //</editor-fold>
+
+}
---
+++ src/gui/controllers/BeheerGroepenPanelController.java
@@ -0,0 +1,174 @@
+package gui.controllers;
+
+import controllers.OefeningController;
+import domein.Oefening;
+import gui.events.AnnuleerEvent;
+import gui.events.DeleteEvent;
+import gui.events.DetailsEvent;
+import gui.events.WijzigEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Comparator;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import utils.AlertCS;
+
+/**
+ * FXML Controller class
+ *
+ * @author sam
+ */
+public class BeheerGroepenPanelController extends AnchorPane {
+
+    @FXML
+    private Button createOefening;
+
+    @FXML
+    private Button detailsBtn;
+
+    @FXML
+    private TableView<Oefening> oefeningenTable;
+
+    @FXML
+    private TableColumn<Oefening, String> opgaveCol;
+
+    @FXML
+    private TableColumn<Oefening, String> vakCol;
+
+    @FXML
+    private TableColumn<Oefening, String> doelstellingenCol;
+
+    @FXML
+    private TextField filterText;
+
+    @FXML
+    private StackPane detailsStackPane;
+
+    private ObservableList<Node> children;
+
+    private OefeningController controller;
+    private Oefening laatstVerwijderd;
+    private SortedList<Oefening> oefeningen;
+
+    public BeheerGroepenPanelController(OefeningController controller) {
+        this.controller = controller;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../panels/BeheerOefeningenPanel.fxml"));
+
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        oefeningen = controller.getOefeningen().sorted(Comparator.comparing(Oefening::getOpgave));
+        children = detailsStackPane.getChildren();
+
+        stelTableViewIn();
+
+        voegEventHandlersToe();
+    }
+
+    @FXML
+    public void createOefeningClicked(ActionEvent event) {
+        children.clear();
+        children.add(new CreateOefeningController(controller));
+    }
+
+    @FXML
+    public void filter(KeyEvent event) {
+        String toFilter = filterText.getText();
+        controller.applyFilter(toFilter);
+
+    }
+
+    private void stelTableViewIn() {
+        opgaveCol.setCellValueFactory(c -> c.getValue().getOpgaveProp());
+        vakCol.setCellValueFactory(c -> c.getValue().getVakProp());
+        doelstellingenCol.setCellValueFactory(c -> c.getValue().getDoelstellingenProp());
+        oefeningenTable.setItems(oefeningen);
+        oefeningenTable.setPlaceholder(new Label("Geen oefeningen"));
+        oefeningenTable.getSelectionModel().selectedItemProperty().addListener((ob, oldval, newval) -> {
+            if (newval != null) {
+                children.clear();
+                children.add(new DetailsOefeningController(newval));
+            }
+        });
+    }
+
+    private void voegEventHandlersToe() {
+        this.addEventHandler(WijzigEvent.WIJZIG, event -> {
+            children.clear();
+            children.add(new CreateOefeningController(controller, event.getId()));
+        });
+
+        this.addEventHandler(DeleteEvent.DELETE, event -> {
+            boolean inBox = controller.zitOefeningInBox(event.getId());
+            laatstVerwijderd = controller.getOefening(event.getId());
+            String opgaveNaam = new File(laatstVerwijderd.getOpgave()).getName();
+            if (inBox) {
+                children.add(new NotificatiePanelController(String.format("Oefening zit nog in een BreakoutBox", opgaveNaam), "#C62828"));
+            } else {
+
+                controller.deleteOefening(event.getId());
+                children.clear();
+                VBox vbox = new VBox();
+                vbox.alignmentProperty().setValue(Pos.CENTER);
+                Button ongedaanButton = new Button("Ongedaan maken");
+                ongedaanButton.setOnAction(clickEvent -> {
+                    controller.createOefening(
+                            laatstVerwijderd.getOpgave(),
+                            laatstVerwijderd.getAntwoord(),
+                            laatstVerwijderd.getFeedback(),
+                            laatstVerwijderd.getVak(),
+                            laatstVerwijderd.getDoelstellingen(),
+                            laatstVerwijderd.getGroepsbewerkingen()
+                    );
+                    oefeningenTable.getSelectionModel().select(controller.getMeestRecenteOefening());               
+                });
+                vbox.getChildren().add(new Label(String.format("Oefening met opgave %s is verwijderd", opgaveNaam)));
+                vbox.getChildren().add(ongedaanButton);
+                children.add(vbox);
+            }
+        });
+
+        this.addEventHandler(DetailsEvent.DETAILS, event -> {
+            children.clear();
+            if (event.getId() < 0) {
+                oefeningenTable.getSelectionModel().select(controller.getMeestRecenteOefening());
+                children.add(new NotificatiePanelController("Oefening is succesvol aangemaakt", "#28BB66"));
+            } else {
+                oefeningenTable.getSelectionModel().select(controller.getOefening(event.getId()));
+                children.add(new NotificatiePanelController("Oefening is succesvol gewijzigd", "#28BB66"));
+            }
+        });
+
+        this.addEventHandler(AnnuleerEvent.ANNULEER, event -> {
+            children.clear();
+            if (event.getId() > 0) {
+                children.add(new DetailsOefeningController(controller.getOefening(event.getId())));
+            }
+        });
+    }
+
+}
